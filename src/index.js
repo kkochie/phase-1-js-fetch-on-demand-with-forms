6cee95130d7805edd52bd62f5a6f617ff57c4d83
Kristine Kochie <kristinekochie@gmail.com>
2022-06-15 09:07:06 -0400

    Solution code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
+    //Add event listener to form element
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (e) => {
+        //Prevent auto refresh on submit
+        e.preventDefault();
+
+        //Access input value from an event object
+        console.log(e.target.children[1].value);
+
+        //Access input value directly
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value);
+
+        //Fetch data
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.textContent = data.title
+            summary.textContent = data.summary
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
